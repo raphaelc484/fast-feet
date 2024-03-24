e4acb0b4918f9b71191fb98a87d93a4a32cfb7c0
raphaelc484 <raphaelc484@hotmail.com>
2024-03-24 10:35:29 -0300

    configuration of tests e2e

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vitest/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  plugins: [tsConfigPaths()],
   test: {
+    environmentMatchGlobs: [['src/infra/http/contollers/**', 'prisma']],
+    dir:'src',
     globals: true,
     root: './',
   },
-  plugins: [tsConfigPaths()],
 })
